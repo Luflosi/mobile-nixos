fd4c70eb140614398524f0852b0b6cb575325c33
Luflosi <luflosi@luflosi.de>
2023-06-23 23:22:04 +0200

    pine64-pinephone: kernel 6.3.0 -> 6.4.0-rc7

--- devices/pine64-pinephone/kernel/default.nix
+++ devices/pine64-pinephone/kernel/default.nix
@@ -6,15 +6,15 @@
 }:
 
 mobile-nixos.kernel-builder {
-  version = "6.3.0";
+  version = "6.4.0-rc7";
   configfile = ./config.aarch64;
   src = fetchFromGitHub {
     # https://github.com/megous/linux
     owner = "megous";
     repo = "linux";
-    # orange-pi-6.3
-    rev = "08ee78c9dfda5effd34848b6f1ae6ce10c466bad";
-    hash = "sha256-hfnBVtWyn6FAAOXnizE4jRaf6b9KYEwlJu3NOD7DMGM=";
+    # orange-pi-6.4
+    rev = "3eff1f0f07768c162592dd401b3ef56bd3619843";
+    hash = "sha256-T47ocU+oYbrvTH+6Tghq+GK9EGeLWTDGQmn1IC7lWpU=";
   };
   patches = [
     ./0001-dts-pinephone-Setup-default-on-and-panic-LEDs.patch
